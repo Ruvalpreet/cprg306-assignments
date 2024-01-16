42fdef1e71119b14c8403b699039cac1288ba3ef
Ruvalpreet <123599263+Ruvalpreet@users.noreply.github.com>
2024-01-16 14:52:03 -0700

    updated main page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+export default function StudentInfo(){
+    return(
+        <>
+            <div>My name is Ruvalpreet</div>
+            <div> my Github <Link href="https://github.com/Ruvalpreet">link</Link>  </div>
+        </>
+    )
+}
